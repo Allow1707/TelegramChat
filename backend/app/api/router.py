b4083a47239558a6c9c8cdd27f947fbdd1a9d52e
Алексей Алпатов <alpatov423@gmail.com>
2025-04-02 10:18:05 +0300

    2025-04-02. refactor: добавление апи методов

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,14 +1,19 @@
-from pydantic import BaseModel
+import json
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.api.schemas import SPartner, SMessge
+from app.api.utils import (
+    send_msg,
+    create_new_room,
+    get_user_info,
+    get_all_rooms_gender,
+    add_user_to_room,
+    refund_partner,
+    is_match,
+)
+from app.dao.fastapi_dao_dep import get_session_without_commit
+from app.redis_dao.custom_redis import CustomRedis
+from app.redis_dao.manager import get_redis
 
+router = APIRouter(prefix="/api", tags=["АПИ"])
 
-class SPartner(BaseModel):
-    id: int
-    age_from: int = 0
-    age_to: int = 999
-    gender: str = "any"
-
-
-class SMessge(BaseModel):
-    sender: str
-    user_id: int
-    message: str
